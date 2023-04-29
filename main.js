8d069a4fffc18f62ebc4bdfc000dbfa22853bd67
Dalton Knuckles <djck244@gmail.com>
2023-04-29 11:50:41 -0700

    adds form to index.html

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-fetch("https://api.weather.gov").then(res => res.JSON).then(data => console.log(data));
+function grabWeatherData() {
+    const city = document.querySelector('input').value
+fetch(`https://goweather.herokuapp.com/weather/${city}`).then(res => res.json()).then(data => console.log(data));
+};
