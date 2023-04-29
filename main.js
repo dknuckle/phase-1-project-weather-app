f07aff4525bf0d7bd743b4a4c0bd1da0dd242e4a
Dalton Knuckles <djck244@gmail.com>
2023-04-29 12:13:09 -0700

    successfully pulls weather data for city entered

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+// initial call to API
 function grabWeatherData() {
     const city = document.querySelector('input').value
-fetch(`https://goweather.herokuapp.com/weather/${city}`).then(res => res.json()).then(data => console.log(data));
+    return fetch(`https://goweather.herokuapp.com/weather/${city}`).then(res => res.json()).then(data => console.log(data));
 };
+
+// event listener for the click to submit
+function lookUpWeather () {
+    const submit = document.querySelector("form")
+    submit.addEventListener('submit', function(e) {
+        e.preventDefault();
+        grabWeatherData();
+    });
+};
+lookUpWeather();
